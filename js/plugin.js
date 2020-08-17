f31dc74df84ab2aee033511f0973ab6ef1d064ea
nabihibrahim <nabih.shrif@gmail.com>
2020-08-17 12:39:05 +0200

    update

--- js/plugin.js
+++ js/plugin.js
@@ -14,14 +14,28 @@ $(document).ready(function(){
         $('.venobox').venobox(); 
     });
 
+    $('.portfolio ul li').on('click',function(){
+        $(this).addClass('active').siblings().removeClass('active')
+    })
+
     //smooth scroll
     $('nav li a').on('click',function(){
 
         //add class active on nav links
-        $(this).addClass('active').parent().siblings().find('a').removeClass('active');
+        $(this).addClass('active').parent().siblings().find('a').removeClass('active')
 
         $('html,body').animate({
             scrollTop : $('#' + $(this).data('scroll')).offset().top -80
+        })
+    })
+
+    $(window).scroll(function(){
+        $('section').each(function(){
+            if ($(window).scrollTop() + 100 >= $(this).offset().top){
+                var secID = $(this).attr('id');
+                $('nav li a').removeClass('active');
+                $('nav li a[data-scroll="'+ secID +'"]').addClass('active')
+            }
         })
     })
 
